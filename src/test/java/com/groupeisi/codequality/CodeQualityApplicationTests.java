0ac58d5952903961f14a3ac5e5a6bc1816bd3537
Boubacar Siddy Diallo <=>
2023-11-14 22:28:18 +0000

    ajout d'un scénario de test au niveau de la classe CodeQualityApplicationTests

--- src/test/java/com/groupeisi/codequality/CodeQualityApplicationTests.java
+++ src/test/java/com/groupeisi/codequality/CodeQualityApplicationTests.java
@@ -1,13 +1,19 @@
 package com.groupeisi.codequality;
 
+import com.groupeisi.codequality.dao.RetraitDao;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class CodeQualityApplicationTests {
 
+	RetraitDao retraitDao = new RetraitDao();
+
 	@Test
 	void contextLoads() {
+		assertEquals(2500, retraitDao.montant());
 	}
 
 }
